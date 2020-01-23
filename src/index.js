a92b881ad2d4d9a5e18b7b7dde16a8c7bf011888
Eli Skeggs <eli@mixmax.com>
2020-01-23 12:39:32 -0800

    style: apply .prettierrc with mixmax dev repo prettier-init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import {Command, flags} from '@oclif/command';
+import { Command, flags } from '@oclif/command';
 
 import cli from 'cli-ux';
 import npm from 'npm';
 import once from 'lodash/once';
-import {promisify} from 'util';
-import {stat} from 'fs';
+import { promisify } from 'util';
+import { stat } from 'fs';
 
-import {check, states} from './check.js';
+import { check, states } from './check.js';
 
 const statAsync = promisify(stat);
 
@@ -21,14 +21,12 @@ const stateMessages = new Map([
 class CheckDependenciesExceptPeerCommand extends Command {
   async run() {
     const {
-      args: {
-        directory: dir = process.cwd(),
-      },
-      flags
+      args: { directory: dir = process.cwd() },
+      flags,
     } = this.parse(CheckDependenciesExceptPeerCommand);
     cli.action.start('initializing');
     if (!(await statAsync(dir)).isDirectory()) {
-      this.error('the provided path is not a directory', {exit: 1});
+      this.error('the provided path is not a directory', { exit: 1 });
     }
     loadNPM();
     const errors = await check(dir, {
@@ -39,7 +37,7 @@ class CheckDependenciesExceptPeerCommand extends Command {
       },
     });
     if (errors && errors.length) {
-      this.error(errors.join('\n'), {exit: 2});
+      this.error(errors.join('\n'), { exit: 2 });
     }
   }
 }
@@ -48,17 +46,20 @@ CheckDependenciesExceptPeerCommand.description = `Verify the lockfile and instal
 Scans the lockfile and installed dependencies for inconsistencies. Ignores peer dependency violations by default.
 `;
 
-CheckDependenciesExceptPeerCommand.args = [{
-  name: 'directory',
-  description: '[default: PWD] path to the root of the module to check, defaults to the working directory',
-  required: false,
-}];
+CheckDependenciesExceptPeerCommand.args = [
+  {
+    name: 'directory',
+    description:
+      '[default: PWD] path to the root of the module to check, defaults to the working directory',
+    required: false,
+  },
+];
 
 CheckDependenciesExceptPeerCommand.flags = {
   // add --version flag to show CLI version
-  version: flags.version({char: 'v'}),
+  version: flags.version({ char: 'v' }),
   // add --help flag to show CLI version
-  help: flags.help({char: 'h'}),
+  help: flags.help({ char: 'h' }),
   'ignore-peer': flags.boolean({
     char: 'i',
     description: 'whether to ignore peer dependencies',
